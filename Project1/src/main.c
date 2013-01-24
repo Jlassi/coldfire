bd9ab10b5a8d631fbce282272d2a0909ab2a0412
Michael Steptoe <michaeltsteptoe@gmail.com>
2013-01-24 08:24:57 -0700

    Steptoe done for now

--- Project1/src/main.c
+++ Project1/src/main.c
@@ -1,3 +1,11 @@
+/****************************************************************
+*   Team Member 1: Ramsey Cant
+*   Team Member 2: Michael Steptoe
+*   CSE325 Embedded Microprocessor Systems
+*   Spring 2013
+*   Lab Project 1
+*   Effort Division- Team Member 1:         Team Member 2:
+*****************************************************************/
 #include <stdio.h>
 #include <stdbool.h>
 #include <math.h>
@@ -15,8 +23,8 @@ CF_LONGWord getBits(CF_LONGWord number, int hpos, int lpos); // Returns the valu
 void setBits(CF_LONGWord *number, int hpos, int lpos, CF_LONGWord value); // Sets the bits from hpos to lpos in integer number to value value. 
 CF_Byte getByte(CF_LONGWord number, int pos); // Returns the byte at position pos. 
 CF_Word getWord(CF_LONGWord number, int pos); // Returns the word at position pos. 
-CF_Byte setByte(CF_LONGWord number, int pos, CF_Byte value); // Returns the byte at position pos to value value. 
-CF_Word setWord(CF_LONGWord number, int pos, CF_Word value); //Returns the word at position pos to value value.
+void setByte(CF_LONGWord *number, int pos, CF_Byte value); // Returns the byte at position pos to value value.
+void setWord(CF_LONGWord *number, int pos, CF_Word value); //Returns the word at position pos to value value.
 
 // Prints a binary representation of a CF_LONGWord
 void printBits(CF_LONGWord *number) {
@@ -55,15 +63,44 @@ bool getBit(CF_LONGWord number, int pos) {
 	return number & (1 << pos);
 }
 
+// Returns the byte at position pos.
+CF_Byte getByte(CF_LONGWord number, int pos){
+    CF_Byte result =0;
+    return result |= number >> pos*8;
+}
+
+// Returns the word at position pos. 
+CF_Word getWord(CF_LONGWord number, int pos){
+    CF_Word result = 0;
+    return result |= number >> pos*16;
+}
+
+
+// Returns the byte at position pos to value value.
+void setByte(CF_LONGWord *number, int pos, CF_Byte value){
+    for(int i=pos*8; i<(pos+1)*8 && i<32; i++){
+        clearBit(number,i);
+    }
+    *number |= value << pos*8;
+}
+
+
+//Returns the word at position pos to value value.
+void setWord(CF_LONGWord *number, int pos, CF_Word value){
+    for(int i =pos*2; i<(pos+1)*8 && i<4; i++){
+        setByte(number,i,0);
+    }
+    *number |= value << pos*16;
+}
+
+
 int main(void) {
 	/*printf("CF_LONGWord size: %i\n", sizeof(CF_LONGWord));
 	printf("CF_Word size: %i\n", sizeof(CF_Word));
 	printf("CF_Byte size: %i\n", sizeof(CF_Byte));*/
 	
-	CF_LONGWord x = 5;
-	
+	CF_LONGWord x =5;
 	printBits(&x);
-	
 	printf("Setting bit at position 4: ");
 	setBit(&x, 4);
 	printBits(&x);
@@ -74,5 +111,31 @@ int main(void) {
 	
 	printf("Bit at position 0: %i\n", (int)getBit(x, 0));
 	printf("Bit at position 1: %i\n", (int)getBit(x, 1));
+    
+    x = pow(2, 31)-1;
+	printBits(&x);
+    CF_Byte y = getByte(x,0);
+    CF_LONGWord test1 = (CF_LONGWord)y;
+    printf("Getting byte at position 0: ");
+    printBits(&test1);
+    
+    
+    CF_Word z = getWord(x,1);
+	CF_LONGWord test2 = (CF_LONGWord)z;
+    printf("Getting Word at position 1: ");
+    printBits(&test2);
+    
+    setByte(&x,0,0);
+    printf("Setting Bye at position 0 to 0 ");
+    printBits(&x);
+    
+    setWord(&x,1,0);
+    printf("Setting Word at position 1 to 0 ");
+    printBits(&x);
+    
+    
+    
+    
+    
 	return 0;
 }
