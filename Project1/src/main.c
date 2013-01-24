6f4565f0eec68374aa5faab0574b172b104ecc74
RamseyK <ramsey.kant@gmail.com>
2013-01-24 09:45:25 -0700

    Modified readme. get and set bits functions

--- Project1/src/main.c
+++ Project1/src/main.c
@@ -11,8 +11,8 @@ void setBit(CF_LONGWord *number, int pos);
 void clearBit(CF_LONGWord *number, int pos);
 void setBitVal(CF_LONGWord *number, int pos, bool value);
 bool getBit(CF_LONGWord number, int pos);
-CF_LONGWord getBits(CF_LONGWord number, int hpos, int lpos); // Returns the value of bits from hpos to lpos in CF_LONGWord number. 
-void setBits(CF_LONGWord *number, int hpos, int lpos, CF_LONGWord value); // Sets the bits from hpos to lpos in integer number to value value. 
+CF_LONGWord getBits(CF_LONGWord number, int hpos, int lpos);
+void setBits(CF_LONGWord *number, int hpos, int lpos, bool value);
 CF_Byte getByte(CF_LONGWord number, int pos); // Returns the byte at position pos. 
 CF_Word getWord(CF_LONGWord number, int pos); // Returns the word at position pos. 
 CF_Byte setByte(CF_LONGWord number, int pos, CF_Byte value); // Returns the byte at position pos to value value. 
@@ -55,17 +55,48 @@ bool getBit(CF_LONGWord number, int pos) {
 	return number & (1 << pos);
 }
 
+// Returns the value of bits from hpos to lpos in CF_LONGWord number.
+CF_LONGWord getBits(CF_LONGWord number, int hpos, int lpos) {
+	CF_LONGWord ret = number;
+	
+	// Clear any bits not within the hpos and lpos range
+	for(int i = sizeof(CF_LONGWord)-1; i >= 0; i--) {
+		if(i > hpos || i < lpos)
+			clearBit(&ret, i);
+	}
+	
+	return ret;
+}
+
+// Sets the bits from hpos to lpos in integer number to value.
+void setBits(CF_LONGWord *number, int hpos, int lpos, bool value) {
+	// Set all bits within the hpos and lpos range
+	for(int i = sizeof(CF_LONGWord)-1; i >= 0; i--) {
+		if((i <= hpos) && (i >= lpos)) {
+			setBitVal(number, i, value);
+		}
+	}
+}
+
 int main(void) {
 	/*printf("CF_LONGWord size: %i\n", sizeof(CF_LONGWord));
 	printf("CF_Word size: %i\n", sizeof(CF_Word));
 	printf("CF_Byte size: %i\n", sizeof(CF_Byte));*/
 	
 	CF_LONGWord x = 5;
+	CF_LONGWord y = 0;
+	printBits(&x);
 	
+	printf("Bits between 3 (high) and 1 (low): ");
+	y = getBits(x, 3, 1);
+	printBits(&y);
+	
+	printf("Set bits between 10 (high) and 7 (low) to 1: ");
+	setBits(&x, 10, 7, true);
 	printBits(&x);
 	
 	printf("Setting bit at position 4: ");
-	setBit(&x, 4);
+	setBitVal(&x, 4, true);
 	printBits(&x);
 	
 	printf("Clearing bit at position 4: ");
--- README.md
+++ README.md
@@ -1,2 +1,14 @@
 coldfire
 ========
+Ramsey Kant, Michael Steptoe
+
+https://github.com/RamseyK/coldfire
+
+Experimental projects to learn the Freescale Coldfire MCF52259 processor
+
+Project 1: Bit manipulation
+
+Prerequisites: CodeWarrior Development Studio v10
+
+License:
+See LICENSE.TXT
