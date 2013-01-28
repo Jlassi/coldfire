5b086d11a0137332055e1d18fafffcbf3b56490e
RamseyK <ramsey.kant@gmail.com>
2013-01-27 20:23:20 -0700

    Changed printBits to print based on the size of the data passed to it. Simplified setByte and setWord to use existing setBits function

--- Project1/src/main.c
+++ Project1/src/main.c
@@ -4,7 +4,7 @@
 *   CSE325 Embedded Microprocessor Systems
 *   Spring 2013
 *   Lab Project 1
-*   Effort Division- Team Member 1:         Team Member 2:
+*   Effort Division- Team Member 1: 50%, Team Member 2: 50%
 *****************************************************************/
 
 #include <stdio.h>
@@ -12,11 +12,12 @@
 #include <math.h>
 
 typedef int CF_LONGWord; 
-#define LONG_WORD_BITS 8*sizeof(CF_LONGWord)
 typedef unsigned char CF_Byte;
 typedef short CF_Word;
+#define LONG_WORD_BITS 8*sizeof(CF_LONGWord)
+#define WORD_BITS 8*sizeof(CF_Word)
 
-void printBits(CF_LONGWord *number);
+void printBits(void *number, int bit_len);
 void setBit(CF_LONGWord *number, int pos);
 void clearBit(CF_LONGWord *number, int pos);
 void setBitVal(CF_LONGWord *number, int pos, bool value);
@@ -29,17 +30,21 @@ void setByte(CF_LONGWord *number, int pos, CF_Byte value); // Returns the byte a
 void setWord(CF_LONGWord *number, int pos, CF_Word value); //Returns the word at position pos to value value.
 
 // Prints a binary representation of a CF_LONGWord
-void printBits(CF_LONGWord *number) {
-	int i = 0;
-	int k = 0;
+void printBits(void *number, int bit_len) {
+	int i = 0, m = 0;
+	int *num = number;
 	
-	for (i = pow(2, (LONG_WORD_BITS)-2); i > 0; i >>= 1) {
-        printf("%i", (((*number) & i) == i) ? 1 : 0);
-        k++;
-    }
+	for (i = bit_len-1; i >= 0; i--) {
+		m = (*num) >> i;
+ 
+		if (m & 1)
+			printf("1");
+		else
+			printf("0");
+	}
+	
 	
 	printf("\n");
-	//printf("Bits: %i\n", k);
 }
 
 // Sets the bit at position pos in CF_LONGWord number
@@ -71,18 +76,19 @@ CF_LONGWord getBits(CF_LONGWord number, int hpos, int lpos) {
 	
 	// Clear any bits not within the hpos and lpos range
 	for(int i = LONG_WORD_BITS-1; i >= 0; i--) {
-		if(i > hpos || i < lpos)
+		if(i > hpos || i < lpos) {
 			clearBit(&ret, i);
+		}
 	}
 	
 	return ret;
 }
 
-// Sets the bits from hpos to lpos in integer number to value.
+// Sets the bits from [lpos, hpos) in integer number to value.
 void setBits(CF_LONGWord *number, int hpos, int lpos, bool value) {
 	// Set all bits within the hpos and lpos range
 	for(int i = LONG_WORD_BITS-1; i >= 0; i--) {
-		if((i <= hpos) && (i >= lpos)) {
+		if((i < hpos) && (i >= lpos)) {
 			setBitVal(number, i, value);
 		}
 	}
@@ -100,21 +106,14 @@ CF_Word getWord(CF_LONGWord number, int pos){
     return result |= number >> pos*16;
 }
 
-
 // Sets the byte at position pos to value.
-void setByte(CF_LONGWord *number, int pos, CF_Byte value){
-    for(int i=pos*8; i<(pos+1)*8 && i<32; i++){
-        clearBit(number,i);
-    }
-    *number |= value << pos*8;
+void setByte(CF_LONGWord *number, int pos, CF_Byte value) {
+	setBits(number, (pos+1)*8, (pos)*8, value);
 }
 
 // Sets the word at position pos to value.
 void setWord(CF_LONGWord *number, int pos, CF_Word value){
-    for(int i =pos*2; i<(pos+1)*8 && i<4; i++){
-        setByte(number,i,0);
-    }
-    *number |= value << pos*16;
+	setBits(number, (pos*8)+WORD_BITS, pos*8, value);
 }
 
 int main(void) {
@@ -125,46 +124,46 @@ int main(void) {
 
 	CF_LONGWord x = 5;
 	CF_LONGWord w = 0;
-	printBits(&x);
+	printBits(&x, LONG_WORD_BITS);
 	
 	printf("Bits between 3 (high) and 1 (low): ");
 	w = getBits(x, 3, 1);
-	printBits(&w);
+	printBits(&w, LONG_WORD_BITS);
 	
 	printf("Set bits between 10 (high) and 7 (low) to 1: ");
 	setBits(&x, 10, 7, true);
-	printBits(&x);
+	printBits(&x, LONG_WORD_BITS);
 	
 	printf("Setting bit at position 4: ");
 	setBitVal(&x, 4, true);
-	printBits(&x);
+	printBits(&x, LONG_WORD_BITS);
 	
 	printf("Clearing bit at position 4: ");
 	clearBit(&x, 4);
-	printBits(&x);
+	printBits(&x, LONG_WORD_BITS);
 	
 	printf("Bit at position 0: %i\n", (int)getBit(x, 0));
 	printf("Bit at position 1: %i\n", (int)getBit(x, 1));
     
-    x = pow(2, 31)-1;
-	printBits(&x);
-    CF_Byte y = getByte(x,0);
-    CF_LONGWord test1 = (CF_LONGWord)y;
-    printf("Getting byte at position 0: ");
-    printBits(&test1);
-    
-    CF_Word z = getWord(x,1);
-	CF_LONGWord test2 = (CF_LONGWord)z;
-    printf("Getting Word at position 1: ");
-    printBits(&test2);
+    x = 4294967295;
+	printf("Number (x) set to: ");
+	printBits(&x, LONG_WORD_BITS);
     
     setByte(&x,0,0);
-    printf("Setting byte at position 0 to 0 ");
-    printBits(&x);
+    printf("Setting byte at position 0 to 0: ");
+    printBits(&x, LONG_WORD_BITS);
+	
+    CF_Byte y = getByte(x,0);
+    printf("Getting byte at position 0: ");
+    printBits(&y, 8);
+    
+    CF_Word z = getWord(x,0);
+    printf("Getting Word at position 0: ");
+    printBits(&z, WORD_BITS);
     
     setWord(&x,1,0);
-    printf("Setting Word at position 1 to 0 ");
-    printBits(&x);
+    printf("Setting Word at position 1 to 0: ");
+    printBits(&x, LONG_WORD_BITS);
     
 	return 0;
 }
